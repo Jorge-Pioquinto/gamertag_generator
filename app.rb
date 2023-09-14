81b748da133bbc1526999979ddb048b12acff8af
Jorge Pioquinto <129575128+Jorge-Pioquinto@users.noreply.github.com>
2023-09-14 02:05:08 +0000

    Base Code Done

--- app.rb
+++ app.rb
@@ -1,9 +1,26 @@
 require "sinatra"
 require "sinatra/reloader"
+require "faker"
+
+GAME_WORDS = {
+  'Minecraft' => [Faker::Games::Minecraft.unique.biome],
+  'Overwatch' => [Faker::Games::Overwatch.unique.hero],
+  'Zelda' => [Faker::Games::Zelda.unique.character],
+  'Star Wars: Battlefront' => [Faker::Movies::StarWars.unique.droid, Faker::Movies::StarWars.unique.call_sign],
+}
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  @games = GAME_WORDS.keys
+  erb :homepage
+end
+
+post '/generate' do
+  selected_game = params['game']
+  word = GAME_WORDS[selected_game].sample
+  username = params['username']
+  user_num = params['user_num']
+
+  @gamertag = "#{username}#{word}#{user_num}".gsub(/\s+/, '')
+
+  erb :result
 end
